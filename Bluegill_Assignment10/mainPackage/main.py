86a5130ed63b409c46e2505ca5b878d04573b301
Ryan Dew <dewrm@mail.uc.edu>
2024-11-10 23:00:24 -0500

    Added data types.

--- Bluegill_Assignment10/mainPackage/main.py
+++ Bluegill_Assignment10/mainPackage/main.py
@@ -14,6 +14,7 @@ import json
 import requests
 
 
+
 url = "https://api.weatherstack.com/current?access_key=2248de933bfdb1b2b7f831ddce40c468"
 
 # Enter the city you want to get the weather data for
@@ -26,12 +27,15 @@ parsed_json = response.json()
 # Create a dictionary to store the weather data
 if "current" in parsed_json:
     weather_data = {
-        "location": parsed_json.get("location", {}).get("name", "Unknown"),
-        "temperature": parsed_json["current"].get("temperature"),
-        "humidity": parsed_json["current"].get("humidity"),
-        "weather_descriptions": parsed_json["current"].get("weather_descriptions", ["No description"]),
-        "wind_speed": parsed_json["current"].get("wind_speed"),
-        "feels_like": parsed_json["current"].get("feelslike"),
+        "LOCATION": parsed_json.get("location", {}).get("name", "Unknown"),
+        "REGION": parsed_json.get("location", {}).get("region", "Unknown"),
+        "TEMPERATURE": parsed_json["current"].get("temperature"),
+        "HUMIDITY": parsed_json["current"].get("humidity"),
+        "WEATHER_DESCRIPTION": parsed_json["current"].get("weather_descriptions"),
+        "WIND_SPEED": parsed_json["current"].get("wind_speed"),
+        "FEELS_LIKE": parsed_json["current"].get("feelslike"),
+        "CLOUD_COVER": parsed_json["current"].get("cloudcover"),
     }
 
 print(weather_data)
+
