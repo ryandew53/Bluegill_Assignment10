ad4f2b9e2a0488c3351563d324d6ad3f26caf926
Ryan Dew <dewrm@mail.uc.edu>
2024-11-10 22:50:43 -0500

    created a dictionary and altered the intital requests code.

--- Bluegill_Assignment10/mainPackage/main.py
+++ Bluegill_Assignment10/mainPackage/main.py
@@ -7,19 +7,31 @@
 # Brief Description of the assignment: 
 
 # Brief Description of what this module does:
-# Citations: 
-# Anything else that's relevant:
+# Citations: https://weatherstack.com/documentation --> API documentation for entering key. 
+# Anything else that's relevant: Used inclass assignment and module 10 slides for reference. 
 
 import json
 import requests
 
 
-
 url = "https://api.weatherstack.com/current?access_key=2248de933bfdb1b2b7f831ddce40c468"
 
-
-querystring = {"query":"New Delhi"}
+# Enter the city you want to get the weather data for
+querystring = {"query":"London"}
 
 response = requests.get(url, params=querystring)
 
-print(response.json())
+parsed_json = response.json()
+
+# Create a dictionary to store the weather data
+if "current" in parsed_json:
+    weather_data = {
+        "location": parsed_json.get("location", {}).get("name", "Unknown"),
+        "temperature": parsed_json["current"].get("temperature"),
+        "humidity": parsed_json["current"].get("humidity"),
+        "weather_descriptions": parsed_json["current"].get("weather_descriptions", ["No description"]),
+        "wind_speed": parsed_json["current"].get("wind_speed"),
+        "feels_like": parsed_json["current"].get("feelslike"),
+    }
+
+print(weather_data)
