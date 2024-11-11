9a3db4ab993b8fc499bae1c24c9f17624fa7d6a6
Ryan Dew <dewrm@mail.uc.edu>
2024-11-10 19:55:53 -0500

    Added project documentation

--- Bluegill_Assignment10/mainPackage/main.py
+++ Bluegill_Assignment10/mainPackage/main.py
@@ -1,1 +1,13 @@
+# Name: Ryan Dew, Kayla Wilson
+# email: dewrm@mail.uc.edu, 
+# Assignment Number: 10
+# Due Date:  11/14/2024
+# Course #/Section: IS4010-001
+# Semester/Year: Fall 2024
+# Brief Description of the assignment: 
+
+# Brief Description of what this module does:
+# Citations: 
+# Anything else that's relevant:
+
 
