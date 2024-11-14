6980bae630ee470285dc5a0519231ecf3ffd155b
Ryan Dew <dewrm@mail.uc.edu>
2024-11-13 19:24:43 -0500

    nothing

--- Bluegill_Assignment10/weather_apiPackage/weather_api.py
+++ Bluegill_Assignment10/weather_apiPackage/weather_api.py
@@ -8,7 +8,7 @@
 # Semester/Year: Fall 2024
 # Brief Description of the assignment: This assignment is to use an API from the web to fetch data, create a dictionary with that data, print it to the terminal, and then finally save that data in a csv file.
 
-# Brief Description of what this module does:
+# Brief Description of what this module does: This 
 # Citations: https://weatherstack.com/documentation --> API documentation for entering key. 
 # Anything else that's relevant: Used inclass assignment and module 10 slides for reference. 
 
